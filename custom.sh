a1f3d7ab11aeef838d2f19126b8b927607400b0e
eysp <60641231+eysp@users.noreply.github.com>
2023-12-05 17:14:00 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.5.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
  
 # add custom packages
 # echo 'src-git xYx https://github.com/xYx-c/openwrt-luci' >>feeds.conf.default
@@ -21,11 +21,17 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git opencla
 mv openclash/luci-app-openclash package/luci-app-openclash && rm -rf openclash
 
 git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kenzok8/small package/small
+svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-npc package/luci-app-npc
 
 git clone --depth=1 -b main https://github.com/kingyond/luci-app-accesscontrol-plus accesscontrol-plus
 mv accesscontrol-plus/luci-app-accesscontrol-plus  package/luci-app-accesscontrol-plus && rm -rf accesscontrol-plus
 
 git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-zerotier package/luci-app-zerotier
+
+# 换源
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/emortal package/emortal
+sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g' package/emortal/default-settings/files/99-default-settings-chinese
 
 # update golang
 # pushd feeds/packages/lang
